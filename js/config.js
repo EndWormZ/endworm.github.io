f3a74fc5814c9712851db26db447357ec6c2fdcd
EndWormZ <128108970+EndWormZ@users.noreply.github.com>
2023-03-17 03:43:40 +0100

    m

--- js/config.js
+++ js/config.js
@@ -1,123 +1,159 @@
-/*    _           _    _                     _ 
-     | |         | |  | |                   | |
-  ___| | ___  ___| | _| |     ___   __ _  __| |
- / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
- \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
- |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
-    version 2.04
-*/
+//       _           _    _                     _ 
+//      | |         | |  | |                   | |
+//   ___| | ___  ___| | _| |     ___   __ _  __| |
+//  / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+//  \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+//  |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+//     version 2.16
 
-/*URL for the server logo
-Leave it blank to use text instead*/
-var l_serverImage = "images/logo.png";
+// URL for the server logo
+// Leave it blank to use text instead
+// (Put the files in the images folder)
+// (You can also use external URL)
+var l_serverImage = "";
 
-/*Custom server name configuration
-Leave it blank for automatic server name*/
-var l_serverName = "GWGRP";
+// Center logo?
+// 'true' or 'false'
+var l_centerLogo = true;
 
-/*Use a video for the background?
-Otherwise it will just use images*/
+// Display current map and gamemode?
+// 'true' or 'false'
+var l_displayMapGamemode = false;
+
+// Custom server name configuration
+// Leave it blank for automatic server name
+var l_serverName = "";
+
+// Use a video for the background?
+// Otherwise it will use images
+// 'true' or 'false'
 var l_bgVideo = false;
 
-/*Video background
-Avaiable options:
-"backgrounds/videos/aurora.webm"
-"backgrounds/videos/bluefall.webm"
-"backgrounds/videos/bluetilefloor.webm"
-"backgrounds/videos/bluewhitecircles.webm"
-"backgrounds/videos/bokehlens.webm"
-"backgrounds/videos/cleanbokeh.webm"
-"backgrounds/videos/coldnight.webm"
-"backgrounds/videos/colorfulburst.webm"
-"backgrounds/videos/dancingstars.webm"
-"backgrounds/videos/dropsflares.webm"
-"backgrounds/videos/focusorangebokeh.webm"
-"backgrounds/videos/glimmering.webm"
-"backgrounds/videos/glowingrings.webm"
-"backgrounds/videos/greencircles.webm"
-"backgrounds/videos/hues.webm"
-"backgrounds/videos/inthedeep.webm"
-"backgrounds/videos/modernflourish.webm"
-"backgrounds/videos/orangeaura.webm"
-"backgrounds/videos/warminside.webm"
-"backgrounds/videos/yellowgreen.webm"
+// Default available videos:
+// "aurora.webm"
+// "bluefall.webm"
+// "bluetilefloor.webm"
+// "bluewhitecircles.webm"
+// "bokehlens.webm"
+// "cleanbokeh.webm"
+// "coldnight.webm"
+// "colorfulburst.webm"
+// "dancingstars.webm"
+// "dropsflares.webm"
+// "focusorangebokeh.webm"
+// "glimmering.webm"
+// "glowingrings.webm"
+// "greencircles.webm"
+// "hues.webm"
+// "inthedeep.webm"
+// "modernflourish.webm"
+// "orangeaura.webm"
+// "warminside.webm"
+// "yellowgreen.webm"
+//
+// You can add your own video backgrounds
+// Just make sure they are using webm VP8 format 
+// (Place the videos in the backgrounds/videos folder)
+// (You can also use external URL)
+var l_background = "";
 
-You can add custom backgrounds
-Just make sure it is at webm VP8 format */
-var l_background = "backgrounds/videos/glimmering.webm";
+// (Image-background only)
+// Enable map-based background image?
+// If enabled, the background will be an image based on the server current map
+// You need to have a background image with the same name as the map
+// Also, the image needs to be in .jpg format
+// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+// 'true' or 'false'
+var l_bgImageMapBased = false;
 
-/*(Image-background only)
-Place one or more images
-Multiple images will automatically shuffle between them*/
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the files in the backgrounds/images folder)
+// (You can also use external URL)
 var l_bgImages = [
-	"backgrounds/images/fond1.jpg",
-	"backgrounds/images/fond2.jpg",
-	"backgrounds/images/fond3.jpg",
-	"backgrounds/images/fond4.jpg",
-	"backgrounds/images/fond5.jpg",
-	"backgrounds/images/fond6.jpg",
-	"backgrounds/images/fond7.jpg",
-	"backgrounds/images/fond8.jpg"
-]
+	"https://ixbt.online/gametech/covers/2020/10/18/cw6xzLDEJo3vEFqH0WzQFUhzrnF2WA5shcPwAaWj.jpeg"
+];
 
-/*Random background images order?*/
+// (Image-background only)
+// Random background images order?
+// 'true' or 'false'
 var l_bgImagesRandom = true;
 
-/*(Image-background only)
-Delay between background images changes
-in milliseconds*/
-var l_bgImageDuration = 5000;
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+var l_bgImageDuration = 60000;
 
-/*(Image-background only)
-Image background fade velocity
-in milliseconds*/
-var l_bgImageFadeVelocity = 2000;
+// (Image-background only)
+// Image background fade velocity
+// in milliseconds
+var l_bgImageFadeVelocity = 3000;
 
-/*Enable background overlay?*/
+// Enable background overlay?
+// 'true' or 'false'
 var l_bgOverlay = true;
 
-/*Background darkening amount
-0 for none (0%), 100 for pitch black (100%)*/
-var l_bgDarkening = 50;
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+var l_bgDarkening = 45;
 
-/*Music player
-'true' or 'false' to enable music in the background*/
+// Play music during the loading screen?
+// 'true' or 'false'
 var l_music = true;
 
-/*Display actual song's name?*/
-var l_musicDisplay = false;
+// Display current music name?
+// 'true' or 'false'
+var l_musicDisplay = true;
 
-/*Music playlist
-Place how much Youtube IDs/.ogg you want*/
+// Music playlist
+// Add as many youtube ids/urls or ogg files you want
+// (Place the ogg files in the music folder)
+// (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-"songs/song.ogg",
+	{youtube: "https://youtu.be/5i4qKemaZG4", name: "Silent Hill"},
 ];
 
-/*Random music order?*/
+// Random music order?
+// 'true' or 'false'
 var l_musicRandom = true;
 
-/*Music volume
-Choose a value between 0 and 100*/
-var l_musicVolume = 25;
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+var l_musicVolume = 15;
 
-/*Enable custom messages?*/
+// Enable custom messages?
+// 'true' or 'false'
 var l_messagesEnabled = true;
 
-/*Enter your custom messages below*/
+// Enter your custom messages below
 var l_messages = [
-"Welcome to GWGRP",
-"Have Fun!",
-"Thanks for Joining GWGRP, Enjoy your stay",
-"to join the pd, you need to become cadet first and wait for a trainer to come"
+    "Не забудьте вступить в дискорд сервера! В нем находится вся необходимая информация о работоспособности и обновлениях сервера! - /discord",
+    "После вступления в игру, не забудьте ознакомиться с внутри-игровыми правилами сервера! - /rules",
+    "Некорректное отображение моделей/текстур ? - /content",
 ];
+// Random message order?
+// 'true' or 'false'
+var l_messagesRandom = true;
 
-/*Random message order?*/
-var l_messagesRandom = false;
+// Delay between message changes
+// in milliseconds
+var l_messagesDelay = 50000;
 
-/*Delay between message changes
-in milliseconds*/
-var l_messagesDelay = 5000;
+// Messages fade time
+// in milliseconds
+var l_messagesFade = 1000;
 
-/*Messages fade time
-in milliseconds*/
-var l_messagesFade = 2000;
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// 'true' or 'false'
+var l_showErrorsIngame = true;
+
+//==============================================================
+//========================== WARNING ===========================
+//==============================================================
+//============ Do not edit anything below this line ============
+//==============================================================
+var checkConfigFile = function() {
+	return true;
+};
